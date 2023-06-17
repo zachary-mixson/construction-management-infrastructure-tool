6be4071dbe8d09b813965dbfe8c4a69ec7a48560
zachary-mixson <zman3939@gmail.com>
2023-06-17 18:31:59 -0400

    Install Node v19.9.0 Create React app Create index.js

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+   const [data, setData] = React.useState(null);
+
+   React.useEffect(() => {
+     fetch("/api")
+         .then((res) => res.json())
+         .then((data) => setData(data.message));
+   }, []);
+
+  return (
+    <div className="App">
+
+      <p>{!data ? "Loading..." : data}</p>
+
+
+    </div>
+  );
+}
+
+export default App;
