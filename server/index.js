e27711808fe9e4127a17ec9ca17004e702ac6530
chafed-rc <ryan.cuff@icloud.com>
2023-06-21 16:30:58 -0400

    Create Routes.js Added app.use on index.js Fixed error when calling userlogins.js functions

--- server/index.js
+++ server/index.js
@@ -2,31 +2,14 @@ const express = require("express")
 const {createUser,deleteUser,changeUsername,updatePassword} = require("./userlogins")
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes/routes");
 
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello World!" });
 });
 
-app.get("/createUser/:username/:password", (req, res) => {
-    createUser(req.params.username, req.params.password);
-    res.send("User created successfully.");
-});
-
-app.get("/deleteUser/:username/:password", (req, res) => {
-    deleteUser(req.params.username, req.params.password);
-    res.send("User deleted successfully.");
-});
-
-app.get("/updatePassword/:username/:currentPassword/:newPassword", (req, res) => {
-    updatePassword(req.params.username, req.params.currentPassword, req.params.newPassword);
-    res.send("Password updated successfully.");
-});
-
-app.get("/changeUsername/:currentUsername/:password/:newUsername", (req, res) => {
-    changeUsername(req.params.currentUsername, req.params.password, req.params.newUsername);
-    res.send("Username changed successfully.");
-});
+app.use('/',routes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
--- server/userlogins.js
+++ server/userlogins.js
@@ -11,53 +11,74 @@ const client = new Client({
 client.connect();
 
 const createUser = (username, password) => {
-    const insertQuery = {
-        text: 'INSERT INTO public.userlogins(username, password) VALUES($1, $2)',
-        values: [username, password],
-    };
+    return new Promise((resolve, reject) => {
+        const insertQuery = {
+            text: 'INSERT INTO public.userlogins(username, password) VALUES($1, $2)',
+            values: [username, password],
+        };
 
-    client.query(insertQuery, (err, res) => {
-        if (err) {
-            console.log(err.stack);
-        }
+        client.query(insertQuery, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+                reject(err); // Reject the promise if there's an error
+            } else {
+                resolve(); // Resolve the promise if the query is successful
+            }
+        });
     });
 };
-const deleteUser = (username, password) => {
-    const deleteQuery = {
-        text: 'DELETE FROM public.userlogins WHERE username = $1 AND password = $2',
-        values: [username, password],
-    };
 
-    client.query(deleteQuery, (err, res) => {
-        if (err) {
-            console.log(err.stack);
-        }
+const deleteUser = (username, password) => {
+    return new Promise((resolve, reject) => {
+        const deleteQuery = {
+            text: 'DELETE FROM public.userlogins WHERE username = $1 AND password = $2',
+            values: [username, password],
+        };
+
+        client.query(deleteQuery, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+                reject(err); // Reject the promise if there's an error
+            } else {
+                resolve(); // Resolve the promise if the query is successful
+            }
+        });
     });
 };
 
 const changeUsername = (currentUsername, password, newUsername) => {
-    const updateQuery = {
-        text: 'UPDATE public.userlogins SET username = $1 WHERE username = $2 AND password = $3',
-        values: [newUsername, currentUsername, password],
-    };
+    return new Promise((resolve, reject) => {
+        const updateQuery = {
+            text: 'UPDATE public.userlogins SET username = $1 WHERE username = $2 AND password = $3',
+            values: [newUsername, currentUsername, password],
+        };
 
-    client.query(updateQuery, (err, res) => {
-        if (err) {
-            console.log(err.stack);
-        }
+        client.query(updateQuery, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+                reject(err); // Reject the promise if there's an error
+            } else {
+                resolve(); // Resolve the promise if the query is successful
+            }
+        });
     });
 };
 
 const updatePassword = (username, currentPassword, newPassword) => {
-    const updateQuery = {
-        text: 'UPDATE public.userlogins SET password = $1 WHERE username = $2 AND password = $3',
-        values: [newPassword, username, currentPassword],
-    };
+    return new Promise((resolve, reject) => {
+        const updateQuery = {
+            text: 'UPDATE public.userlogins SET password = $1 WHERE username = $2 AND password = $3',
+            values: [newPassword, username, currentPassword],
+        };
 
-    client.query(updateQuery, (err, res) => {
-        if (err) {
-            console.log(err.stack);
-        }
+        client.query(updateQuery, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+                reject(err); // Reject the promise if there's an error
+            } else {
+                resolve(); // Resolve the promise if the query is successful
+            }
+        });
     });
 };
 
