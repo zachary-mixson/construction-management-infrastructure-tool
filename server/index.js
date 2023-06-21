cbdbc7ef71b5867f5778d8497a12f4b3b80ab109
chafed-rc <ryan.cuff@icloud.com>
2023-06-21 16:33:38 -0400

    Updated index.js file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const {createUser,deleteUser,changeUsername,updatePassword} = require("./userlogins")
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes/routes");
